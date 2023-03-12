ffb3654b359a8769394cdea7bdc486cfcd17afea
LutSerg <5QGJAZCqesdHUvk>
2023-03-12 19:09:53 +0300

    first commit

---
+++ src/test/java/com/asteroster/solutionstest/SolutionsTest.java
@@ -0,0 +1,28 @@
+package com.asteroster.solutionstest;
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SolutionsTest {
+    @BeforeEach
+    void setUp() {
+        Configuration.browser = "firefox";
+    }
+    @Test
+    void solutionTest () {
+
+        Configuration.holdBrowserOpen = true;
+        open("https://github.com/");
+        $$("ul .HeaderMenu-link").findBy(Condition.text("Solutions")).hover();
+        $(byText("Enterprise")).click();
+        $(".h1-mktg").shouldBe(Condition.visible)
+                .shouldHave(Condition.text("Build like the best"));
+    }
+}
